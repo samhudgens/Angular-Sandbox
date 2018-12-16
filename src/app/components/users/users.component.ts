708d03316f67a62989f7512a006344cde3bd6e35
Sam H <sam@Sams-MacBook-Pro.local>
2018-12-16 17:03:33 -0500

    build form with events and bindings

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,58 +8,100 @@ import { User } from '../../models/User';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  }
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
+  showUserForm: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 30,
-          address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'MA'
-          },
-          isActive: true,
-          registered: new Date('01/02/2018 08:30:00')
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA'
         },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          age: 34,
-          address: {
-            street: '20 School st',
-            city: 'Lynn',
-            state: 'MA'
-          },
-          isActive: false,
-          registered: new Date('03/11/2017 06:20:00')
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA'
         },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 31,
-          address: {
-            street: '12 Barf st',
-            city: 'Miami',
-            state: 'FL'
-          },
-          isActive: true,
-          registered: new Date('11/02/2016 10:30:00')
-        }
-      ];
+        isActive: false,
+        registered: new Date('03/11/2017 06:20:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 31,
+        address: {
+          street: '12 Barf st',
+          city: 'Miami',
+          state: 'FL'
+        },
+        isActive: true,
+        registered: new Date('11/02/2016 10:30:00'),
+        hide: true
+      }
+    ];
 
-      this.loaded = true;
+    this.loaded = true;
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    
+    this.users.unshift(this.user);
+
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      address: {
+        street: '',
+        city: '',
+        state: ''
+      }
+    }
+  }
+
+  toggleHide(user: User) {
+    user.hide = !user.hide;
+  }
+
+  onSubmit(e) {
+    console.log(123);
+    e.preventDefault();
+  }
+
+  fireEvent(e) {
+    console.log(e.type);
+    console.log(e.target.value);
   }
 
 }
